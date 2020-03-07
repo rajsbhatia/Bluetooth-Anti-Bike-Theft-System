176c0faaabf9b6a5ef79ba4d92cf4966f7b61a16
Mcdeezy <zackmcd@campus-030-096.ucdavis.edu>
2020-03-07 12:07:05 -0800

    new mpu test

---
+++ mpuNRF/mpuNRF.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+const int mpuAddr = 0x68;
+int16_t axisX, axisY, axisZ;
+
+int minVal = 265;
+int maxVal = 402;
+
+double x;
+double y;
+double z;
+
+void setup() {
+  Wire.begin();
+  Wire.beginTransmission(mpuAddr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Wire.beginTrandmission(mpuAddr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(mpuAddr, 14, true);
+  
+  axisX = Wire.read()<<8|Wire.read();
+  axisY = Wire.read()<<8|Wire.read();
+  axisZ = Wire.read()<<8|Wire.read();
+
+  int xAng = map(axisX, minVal, MaxVal, -90, 90);
+  int yAng = map(axisY, minVal, MaxVal, -90, 90);
+  int zAng = map(axisZ, minVal, MaxVal, -90, 90);
+
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+  Serial.println("***********");
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(z);
+  delay(1000);
+}
